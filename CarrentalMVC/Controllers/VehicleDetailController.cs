cf6c2df41312c9f01bfc1096fc6100f848fabc9c
FahadLashari <mfhlashari@live.com>
2018-10-27 01:21:47 +0500

    First Commit

---
+++ CarrentalMVC/Controllers/VehicleDetailController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarrentalMVC.Controllers
+{
+    public class VehicleDetailController : Controller
+    {
+        // GET: VehicleDetail
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddVehicle()
+        {
+            return View();
+        }
+    }
+}
